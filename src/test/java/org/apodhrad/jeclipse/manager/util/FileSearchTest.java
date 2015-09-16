b50acbaf6c7d28b2a8e41c4e729c9ee0d65cb57d
Andrej Podhradsky <apodhrad@redhat.com>
2015-09-16 12:29:52 +0200

    Added test for FileSearch

--- src/test/java/org/apodhrad/jeclipse/manager/util/FileSearchTest.java
+++ src/test/java/org/apodhrad/jeclipse/manager/util/FileSearchTest.java
@@ -1,5 +1,13 @@
 package org.apodhrad.jeclipse.manager.util;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apodhrad.jeclipse.manager.matcher.FileNameStartsWith;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -11,17 +19,18 @@ public class FileSearchTest {
 	public TemporaryFolder folder = new TemporaryFolder();
 
 	@Before
-	public void before() {
-		System.out.println("\t->" + folder.getRoot().getAbsolutePath());
+	public void before() throws IOException {
+		folder.newFile("test1.txt");
+		File dir = folder.newFolder("dir");
+		File.createTempFile("test2.txt", null, dir);
 	}
-	
+
 	@Test
 	public void findTest() {
-		
-	}
-	
-	@Test
-	public void find2Test() {
-		
+		FileSearch fileSearch = new FileSearch();
+		List<File> files = fileSearch.find(folder.getRoot(), new FileNameStartsWith("test"));
+		assertEquals(2, files.size());
+		assertTrue(files.get(0).getName().startsWith("test"));
+		assertTrue(files.get(1).getName().startsWith("test"));
 	}
 }
