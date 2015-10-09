c2226615ad8556b3731b684fff1c071c29ae563b
Andrej Podhradsky <apodhrad@redhat.com>
2015-10-09 17:00:21 +0200

    Add basic tests for jeclipse-maven-plugin

---
+++ jeclipse-maven-plugin/src/test/java/org/apodhrad/jeclipse/maven/plugin/InstallerTest.java
@@ -0,0 +1,108 @@
+package org.apodhrad.jeclipse.maven.plugin;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Properties;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.shared.invoker.DefaultInvocationRequest;
+import org.apache.maven.shared.invoker.DefaultInvoker;
+import org.apache.maven.shared.invoker.InvocationRequest;
+import org.apache.maven.shared.invoker.InvocationResult;
+import org.apache.maven.shared.invoker.Invoker;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class InstallerTest {
+
+	private static String TARGET;
+	private static String JBDS_URL;
+	private static String JBDS_SHA256;
+
+	@BeforeClass
+	public static void beforeClass() throws IOException {
+		TARGET = systemProperty("project.build.directory");
+		JBDS_URL = systemProperty("jeclipse.test.jbds.url");
+		JBDS_SHA256 = systemProperty("jeclipse.test.jbds.sha256");
+	}
+
+	@Test
+	public void installEclipseLunaTest() throws Exception {
+		File pomFile = prepareMavenProject("install-eclipse-luna");
+
+		InvocationRequest request = new DefaultInvocationRequest();
+		request.setPomFile(pomFile);
+		request.setGoals(Collections.singletonList("package"));
+
+		Properties systemProperties = new Properties();
+		systemProperties.put("jeclipse.version", System.getProperty("jeclipse.version"));
+		request.setProperties(systemProperties);
+
+		Invoker invoker = new DefaultInvoker();
+		InvocationResult result = invoker.execute(request);
+
+		int exitCode = result.getExitCode();
+		assertEquals("Build failed (exit code " + exitCode + ")", 0, exitCode);
+	}
+
+	@Test
+	public void installEclipseMarsTest() throws Exception {
+		File pomFile = prepareMavenProject("install-eclipse-mars");
+
+		InvocationRequest request = new DefaultInvocationRequest();
+		request.setPomFile(pomFile);
+		request.setGoals(Collections.singletonList("package"));
+
+		Properties systemProperties = new Properties();
+		systemProperties.put("jeclipse.version", System.getProperty("jeclipse.version"));
+		request.setProperties(systemProperties);
+
+		Invoker invoker = new DefaultInvoker();
+		InvocationResult result = invoker.execute(request);
+
+		int exitCode = result.getExitCode();
+		assertEquals("Build failed (exit code " + exitCode + ")", 0, exitCode);
+	}
+
+	@Test
+	public void installJBDSTest() throws Exception {
+		File pomFile = prepareMavenProject("install-jbds");
+
+		InvocationRequest request = new DefaultInvocationRequest();
+		request.setPomFile(pomFile);
+		request.setGoals(Collections.singletonList("package"));
+
+		Properties systemProperties = new Properties();
+		systemProperties.put("jeclipse.version", System.getProperty("jeclipse.version"));
+		systemProperties.put("jeclipse.test.jbds.url", JBDS_URL);
+		systemProperties.put("jeclipse.test.jbds.sha256", JBDS_SHA256);
+		request.setProperties(systemProperties);
+
+		Invoker invoker = new DefaultInvoker();
+		InvocationResult result = invoker.execute(request);
+
+		int exitCode = result.getExitCode();
+		assertEquals("Build failed (exit code " + exitCode + ")", 0, exitCode);
+	}
+
+	static public String systemProperty(String key) {
+		String value = System.getProperty(key);
+		assertTrue("The system property '" + key + "' must be defined!", value != null && value.length() > 0);
+		return value;
+	}
+
+	private File prepareMavenProject(String name) throws IOException {
+		URL url = InstallerTest.class.getResource("/" + name + ".xml");
+		File target = new File(TARGET, name);
+		target.mkdir();
+		File pomFile = new File(target, "pom.xml");
+		FileUtils.copyURLToFile(url, pomFile);
+		return pomFile;
+	}
+
+}
