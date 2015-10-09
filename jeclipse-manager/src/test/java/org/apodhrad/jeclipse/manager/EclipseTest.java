c700f4d6fe3e47dfe0c4a4840f133e5252fb2c6b
Andrej Podhradsky <apodhrad@redhat.com>
2015-10-09 16:58:47 +0200

    Add a simple test for checking md5 check sums

--- jeclipse-manager/src/test/java/org/apodhrad/jeclipse/manager/EclipseTest.java
+++ jeclipse-manager/src/test/java/org/apodhrad/jeclipse/manager/EclipseTest.java
@@ -234,6 +234,13 @@ public class EclipseTest {
 		}
 		assertTrue("Not all VM arguments were added!", foundFoo1 && foundFoo2);
 	}
+	
+	@Test
+	public void md5HashTableTest() {
+		for (String value: Eclipse.ECLIPSE_MD5.values()) {
+			assertEquals(32, value.length());
+		}
+	}
 
 	private static void assertContainsBundle(List<Bundle> bundles, String expectedName, String expectedVersion) {
 		Bundle bundle = new Bundle(expectedName, expectedVersion);
