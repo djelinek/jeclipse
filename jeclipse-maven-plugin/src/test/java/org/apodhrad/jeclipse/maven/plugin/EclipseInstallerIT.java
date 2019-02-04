2e2b4a8a9fa7c5ceb7bc90547799f7a004c21b2c
Andrej Podhradsky <apodhrad@redhat.com>
2019-02-04 13:22:00 +0100

    Update elipse integration tests on macosx
    Signed-off-by: Andrej Podhradsky <apodhrad@redhat.com>

--- jeclipse-maven-plugin/src/test/java/org/apodhrad/jeclipse/maven/plugin/EclipseInstallerIT.java
+++ jeclipse-maven-plugin/src/test/java/org/apodhrad/jeclipse/maven/plugin/EclipseInstallerIT.java
@@ -6,69 +6,38 @@ import java.util.List;
 
 import org.apodhrad.jeclipse.manager.Bundle;
 import org.apodhrad.jeclipse.manager.Eclipse;
+import org.apodhrad.jeclipse.manager.util.OS;
 import org.junit.Test;
 
 public class EclipseInstallerIT extends BetterAbstractMojoTestCase {
 
 	@Test
 	public void testInstallEclipseJEELunaSR2Test() throws Exception {
-		File pom = getTestFile("src/test/resources/install-eclipse-jee-luna-SR2-test/pom.xml");
-		assertNotNull(pom);
-		assertTrue(pom.exists());
-
-		Installer installer = (Installer) lookupConfiguredMojo("install", pom);
-		assertNotNull(installer);
-		installer.execute();
-
-		Eclipse eclipse = new Eclipse(new File(pom.getParentFile(), "target/eclipse"));
-		assertContainsBundle(eclipse.getPlugins(), "org.eclipse.equinox.launcher", "1.3.0.v20140415-2008");
+		testInstallEclipse("jee-luna-SR2", "1.3.0.v20140415-2008");
 	}
 
 	@Test
 	public void testInstallEclipseJEEMars1Test() throws Exception {
-		File pom = getTestFile("src/test/resources/install-eclipse-jee-mars-1-test/pom.xml");
-		assertNotNull(pom);
-		assertTrue(pom.exists());
-
-		Installer installer = (Installer) lookupConfiguredMojo("install", pom);
-		assertNotNull(installer);
-		installer.execute();
-
-		Eclipse eclipse = new Eclipse(new File(pom.getParentFile(), "target/eclipse"));
-		assertContainsBundle(eclipse.getPlugins(), "org.eclipse.equinox.launcher", "1.3.100.v20150511-1540");
+		testInstallEclipse("jee-mars-1", "1.3.100.v20150511-1540");
 	}
 
 	@Test
 	public void testInstallEclipseJEENeon3Test() throws Exception {
-		File pom = getTestFile("src/test/resources/install-eclipse-jee-neon-3-test/pom.xml");
-		assertNotNull(pom);
-		assertTrue(pom.exists());
-
-		Installer installer = (Installer) lookupConfiguredMojo("install", pom);
-		assertNotNull(installer);
-		installer.execute();
-
-		Eclipse eclipse = new Eclipse(new File(pom.getParentFile(), "target/eclipse"));
-		assertContainsBundle(eclipse.getPlugins(), "org.eclipse.equinox.launcher", "1.3.201.v20161025-1711");
+		testInstallEclipse("jee-neon-3", "1.3.201.v20161025-1711");
 	}
 
 	@Test
 	public void testInstallEclipseJEEOxygen3aTest() throws Exception {
-		File pom = getTestFile("src/test/resources/install-eclipse-jee-oxygen-3a-test/pom.xml");
-		assertNotNull(pom);
-		assertTrue(pom.exists());
-
-		Installer installer = (Installer) lookupConfiguredMojo("install", pom);
-		assertNotNull(installer);
-		installer.execute();
-
-		Eclipse eclipse = new Eclipse(new File(pom.getParentFile(), "target/eclipse"));
-		assertContainsBundle(eclipse.getPlugins(), "org.eclipse.equinox.launcher", "1.4.0.v20161219-1356");
+		testInstallEclipse("jee-oxygen-3a", "1.4.0.v20161219-1356");
 	}
 
 	@Test
 	public void testInstallEclipseJEEPhotonRTest() throws Exception {
-		File pom = getTestFile("src/test/resources/install-eclipse-jee-photon-R-test/pom.xml");
+		testInstallEclipse("jee-photon-R", "1.5.0.v20180512-1130");
+	}
+
+	private void testInstallEclipse(String eclipseVersion, String expectedLauncherVersion) throws Exception {
+		File pom = getTestFile("src/test/resources/install-eclipse-jee-" + eclipseVersion + "-test/pom.xml");
 		assertNotNull(pom);
 		assertTrue(pom.exists());
 
@@ -76,8 +45,12 @@ public class EclipseInstallerIT extends BetterAbstractMojoTestCase {
 		assertNotNull(installer);
 		installer.execute();
 
-		Eclipse eclipse = new Eclipse(new File(pom.getParentFile(), "target/eclipse"));
-		assertContainsBundle(eclipse.getPlugins(), "org.eclipse.equinox.launcher", "1.5.0.v20180512-1130");
+		String eclipseHome = "target/eclipse";
+		if (OS.isMac()) {
+			eclipseHome = "target/Ecpipse.app";
+		}
+		Eclipse eclipse = new Eclipse(new File(pom.getParentFile(), eclipseHome));
+		assertContainsBundle(eclipse.getPlugins(), "org.eclipse.equinox.launcher", expectedLauncherVersion);
 	}
 
 	private static void assertContainsBundle(List<Bundle> bundles, String expectedName, String expectedVersion) {
